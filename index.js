551b0ad2b4dd16e65c29c0eef42af6061875e4a0
Andrei Palii <andrei.palii@endava.com>
2024-04-08 16:07:50 +0300

    Add graph visualization using SVG + add print method to Graph + format code

--- index.js
+++ index.js
@@ -2,74 +2,73 @@
 
 // Вектор должен поддерживать интерфейс двусторонней очереди, как у нативных массивов JS.
 
-  //  ```js
-  
-  // class Vector {
-  //   constructor(typedArrayFn, options) {
-  //     let vector = null
-  //     switch (typedArrayFn) {
-  //       case Uint8Array:
-  //         const memorySpace = new ArrayBuffer(options.capacity )
-  //          vector = new Uint8Array(memorySpace)
-  //         break;
-      
-  //       default:
-  //         console.log("Wrong params passed to Vector contructor!");
-  //         break;
-  //     }
-  //     console.log("Created=", vector);
-  //     this.vector = vector
-  //     this.length = 0
-  //   }
+//  ```js
 
-  //   push(...values) {
-  //     for (let value of values) {
-  //       this.vector[this.length] = value
-  //       this.length = this.length + 1
-  //     }
-  //     console.log(this.length)
-  //   }
+// class Vector {
+//   constructor(typedArrayFn, options) {
+//     let vector = null
+//     switch (typedArrayFn) {
+//       case Uint8Array:
+//         const memorySpace = new ArrayBuffer(options.capacity )
+//          vector = new Uint8Array(memorySpace)
+//         break;
 
-  //   pop() {
-  //     const popedValue = this.vector[this.length-1] 
-  //     this.vector[this.length-1] = 0
-  //     this.length = this.length - 1
-  //     console.log(popedValue);
-  //   }
+//       default:
+//         console.log("Wrong params passed to Vector contructor!");
+//         break;
+//     }
+//     console.log("Created=", vector);
+//     this.vector = vector
+//     this.length = 0
+//   }
 
-  //   shift() {
-  //     const shiftedValue = this.vector[0]
-  //     for(let i = 0; i <= this.length - 1; i++) {
-  //       this.vector[i] = this.vector[i + 1]
-  //     }
-  //     this.length = this.length - 1
-  //     console.log(shiftedValue);
-  //   }
+//   push(...values) {
+//     for (let value of values) {
+//       this.vector[this.length] = value
+//       this.length = this.length + 1
+//     }
+//     console.log(this.length)
+//   }
 
-  //   unshift(value) {
-  //     let savedValue = this.vector[0]
-  //     this.vector[0] = value
-  //     for(let i = 1; i <= this.length; i++) {
-  //       this.vector[i] = savedValue
-  //       savedValue = this.vector[i + 1]
-  //     }
-  //     this.length = this.length + 1
-  //     console.log(this.length);
-  //   }
-  //  }
+//   pop() {
+//     const popedValue = this.vector[this.length-1]
+//     this.vector[this.length-1] = 0
+//     this.length = this.length - 1
+//     console.log(popedValue);
+//   }
 
+//   shift() {
+//     const shiftedValue = this.vector[0]
+//     for(let i = 0; i <= this.length - 1; i++) {
+//       this.vector[i] = this.vector[i + 1]
+//     }
+//     this.length = this.length - 1
+//     console.log(shiftedValue);
+//   }
 
-  //  const uint8Vector = new Vector(Uint8Array, {capacity: 3});
+//   unshift(value) {
+//     let savedValue = this.vector[0]
+//     this.vector[0] = value
+//     for(let i = 1; i <= this.length; i++) {
+//       this.vector[i] = savedValue
+//       savedValue = this.vector[i + 1]
+//     }
+//     this.length = this.length + 1
+//     console.log(this.length);
+//   }
+//  }
 
-  //  uint8Vector.push(100);    // 1 --- [100]
-  //  uint8Vector.push(20, 10); // 3 --- [100, 20, 10]
+//  const uint8Vector = new Vector(Uint8Array, {capacity: 3});
 
-  //  uint8Vector.pop();        // 10 --- [100, 20]
-  //  uint8Vector.shift();      // 100 --- [20]
+//  uint8Vector.push(100);    // 1 --- [100]
+//  uint8Vector.push(20, 10); // 3 --- [100, 20, 10]
 
-  //  uint8Vector.unshift(1);          // 2 --- [1, 20]
-  //  console.log(uint8Vector.length); // 2
-  //  ```
+//  uint8Vector.pop();        // 10 --- [100, 20]
+//  uint8Vector.shift();      // 100 --- [20]
+
+//  uint8Vector.unshift(1);          // 2 --- [1, 20]
+//  console.log(uint8Vector.length); // 2
+//  ```
 
 // //   ## Сжатие глубокого объекта
 
@@ -96,25 +95,23 @@
 //   }
 //   return result
 // }
-   
-   
+
 //   console.log(collapse(obj)); /* {'a.b.0': 1, 'a.b.1': 2, 'a..c': 2} */
 //   console.log(collapse({ c: 5, b: {a: 5}})) // { c: 5, 'b.a': 5 }
 //   //  ```
-
 
 //   ## Сжатие глубокого объекта
 
 // Необходимо написать функцию, которая бы сжимала некоторый глубокий объект в плоский вид.
 // Задача должна быть решена минимум двумя способами: через рекурсию и через стек. Можно, также, решить через очередь.
 
-  //  ```js
-  // const obj = {
-  //   a: {
-  //     b: [1, 2],
-  //     '': {c: 2}
-  //   }
-  // };
+//  ```js
+// const obj = {
+//   a: {
+//     b: [1, 2],
+//     '': {c: 2}
+//   }
+// };
 
 //   function collapse(obj) {
 //    const result = {}
@@ -145,42 +142,39 @@
 
 //   return result;
 // }
-  
-  
+
 //  console.log(collapseStack(obj)); /* {'a.b.0': 1, 'a.b.1': 2, 'a..c': 2} */
 //  console.log(collapseStack({ c: 5, b: {a: 5}})) // { c: 5, 'b.a': 5 }
- //  ```
+//  ```
 
 //  ## Валидация скобочных групп
 
 // Необходимо написать функцию, которая бы принимала строку и возвращала true, если у каждого из символов `{`, `[` и `(` есть своя закрывающая пара и они стоят в правильной последовательности.
 
-  // //  ```js
-  // const isValid = (str) => {
-  //   const stack = []
-  //   const openers = ['(', '{', '[']
-  //   const closers = [')', '}', ']']
-  //   for (let char of str) {
-  //     if(openers.includes(char)) {
-  //       stack.push(char)
-  //     }
+// //  ```js
+// const isValid = (str) => {
+//   const stack = []
+//   const openers = ['(', '{', '[']
+//   const closers = [')', '}', ']']
+//   for (let char of str) {
+//     if(openers.includes(char)) {
+//       stack.push(char)
+//     }
 
-      
-  //     if(closers.includes(char)) {
-  //       const indexOfCloser = closers.indexOf(char)
-  //       const indexOfOpener = openers.indexOf(stack[stack.length-1])
-  //       if(indexOfOpener !== indexOfCloser) return false
-  //       stack.pop()
-  //     }
-  //   }
-  //   return stack.length === 0
-  // }
+//     if(closers.includes(char)) {
+//       const indexOfCloser = closers.indexOf(char)
+//       const indexOfOpener = openers.indexOf(stack[stack.length-1])
+//       if(indexOfOpener !== indexOfCloser) return false
+//       stack.pop()
+//     }
+//   }
+//   return stack.length === 0
+// }
 
-  //  console.log(isValid('(hello{world} and [me])'));  // true
-  //  console.log(isValid('(hello{world)} and [me])')); // false
-  //  console.log(isValid(')'));                        // false
-  //  ```
-
+//  console.log(isValid('(hello{world} and [me])'));  // true
+//  console.log(isValid('(hello{world)} and [me])')); // false
+//  console.log(isValid(')'));                        // false
+//  ```
 
 // ## Реализовать вектор над типизированным массивом
 
@@ -197,9 +191,9 @@
 //   }
 
 //   #extendArray() {
-//     const newCapacity = this.capacity * 2; 
+//     const newCapacity = this.capacity * 2;
 //     const newArray = new this.array.constructor(newCapacity);
-//     newArray.set(this.array); 
+//     newArray.set(this.array);
 //     this.array = newArray;
 //     this.capacity = newCapacity;
 //   }
@@ -234,7 +228,7 @@
 //     console.log(shiftedValue);
 //     return shiftedValue
 //   }
-   
+
 //   unshift(...values) {
 //     if(this.length+values.length > this.capacity) {
 //       this.#extendArray()
@@ -263,55 +257,54 @@
 // ## Реализовать класс для описания 3-х мерной матрицы
 
 //    ```js
-  //  const matrix = new Matrix3D({x: 10, y: 10, z: 10});
- // implementarea e ChatGPT-ului, am fc skip la exercitiul acesta ;)
-  //  class Matrix3D {
-  //   constructor(dimensions) {
-  //     this.dimensions = dimensions;
-  //     this.data = new Array(dimensions.x);
-  //     for (let i = 0; i < dimensions.x; i++) {
-  //       this.data[i] = new Array(dimensions.y);
-  //       for (let j = 0; j < dimensions.y; j++) {
-  //         this.data[i][j] = new Array(dimensions.z).fill(0);
-  //       }
-  //     }
-  //   }
-  
-  //   set(coordinates, value) {
-  //     const { x, y, z } = coordinates;
-  //     if (this.isValidIndex(x, y, z)) {
-  //       this.data[x][y][z] = value;
-  //     } else {
-  //       console.error("Invalid coordinates");
-  //     }
-  //   }
-  
-  //   get(coordinates) {
-  //     const { x, y, z } = coordinates;
-  //     if (this.isValidIndex(x, y, z)) {
-  //       return this.data[x][y][z];
-  //     } else {
-  //       console.error("Invalid coordinates");
-  //       return undefined;
-  //     }
-  //   }
-  
-  //   isValidIndex(x, y, z) {
-  //     return x >= 0 && x < this.dimensions.x &&
-  //            y >= 0 && y < this.dimensions.y &&
-  //            z >= 0 && z < this.dimensions.z;
-  //   }
-  // }
-  
-  // Example usage
-  // const matrix = new Matrix3D({ x: 10, y: 10, z: 10 });
-  
-  // matrix.set({ x: 1, y: 3, z: 2 }, 10);
-  // console.log(matrix.get({ x: 1, y: 3, z: 2 })); // Output: 10
-  
+//  const matrix = new Matrix3D({x: 10, y: 10, z: 10});
+// implementarea e ChatGPT-ului, am fc skip la exercitiul acesta ;)
+//  class Matrix3D {
+//   constructor(dimensions) {
+//     this.dimensions = dimensions;
+//     this.data = new Array(dimensions.x);
+//     for (let i = 0; i < dimensions.x; i++) {
+//       this.data[i] = new Array(dimensions.y);
+//       for (let j = 0; j < dimensions.y; j++) {
+//         this.data[i][j] = new Array(dimensions.z).fill(0);
+//       }
+//     }
+//   }
 
-  //  matrix.set({x: 1, y: 3, z: 2}, 10);
-  //  matrix.get({x: 1, y: 3, z: 2});
+//   set(coordinates, value) {
+//     const { x, y, z } = coordinates;
+//     if (this.isValidIndex(x, y, z)) {
+//       this.data[x][y][z] = value;
+//     } else {
+//       console.error("Invalid coordinates");
+//     }
+//   }
+
+//   get(coordinates) {
+//     const { x, y, z } = coordinates;
+//     if (this.isValidIndex(x, y, z)) {
+//       return this.data[x][y][z];
+//     } else {
+//       console.error("Invalid coordinates");
+//       return undefined;
+//     }
+//   }
+
+//   isValidIndex(x, y, z) {
+//     return x >= 0 && x < this.dimensions.x &&
+//            y >= 0 && y < this.dimensions.y &&
+//            z >= 0 && z < this.dimensions.z;
+//   }
+// }
+
+// Example usage
+// const matrix = new Matrix3D({ x: 10, y: 10, z: 10 });
+
+// matrix.set({ x: 1, y: 3, z: 2 }, 10);
+// console.log(matrix.get({ x: 1, y: 3, z: 2 })); // Output: 10
+
+//  matrix.set({x: 1, y: 3, z: 2}, 10);
+//  matrix.get({x: 1, y: 3, z: 2});
 //    ```
 
 // ## Реализовать класс для создания хэш-таблицы
@@ -321,7 +314,6 @@
 
 //    ```js
 //    // Задаем ёмкость внутреннего буфера
-
 
 // class HashMap {
 //   constructor(sizeBytes) {
@@ -369,7 +361,7 @@
 //    map.set('foo', 1);
 //    map.set(42, 10);
 //    map.set(document, 100);
-   
+
 //    console.log(map.get(42));          // 10
 //    console.log(map.has(document));    // true
 //    console.log(map.delete(document)); // 10
@@ -387,6 +379,9 @@
 // Сделать несколько вариантов для орграфов и неорграфов. Визуализировать через SVG разметку как изображение.
 
 // -----------------------------------------------
+
+// realizare prin matrix
+
 // class MatrixForGraph {
 //   constructor(vertexAmount) {
 //     this.vertexAmount = vertexAmount
@@ -447,56 +442,108 @@
 // myMatrix.addVertex()
 // myMatrix.print()
 
-
 // -----------------------------------------------
 
-class Graph {
-  constructor(isDirected = false) {
-    this.graph = new Map()
-    this.isDirected = isDirected
-  }
+// class Graph {
+//   constructor(isDirected = false) {
+//     this.graph = new Map();
+//     this.isDirected = isDirected;
+//   }
 
-  addVertex(vertexValue) {
-    if(!this.graph.has(vertexValue)) {
-      this.graph.set(vertexValue, [])
-    } else {
-      console.log("This node already exist in the graph!")
-    }
-  }
+//   addVertex(vertexValue) {
+//     if (!this.graph.has(vertexValue)) {
+//       this.graph.set(vertexValue, []);
+//     } else {
+//       console.log("This node already exist in the graph!");
+//     }
+//   }
 
-  addEdge(vertex1, vertex2) {
-    const areNodesExisting = this.graph.has(vertex1) && this.graph.has(vertex2)
+//   addEdge(vertex1, vertex2) {
+//     const areNodesExisting = this.graph.has(vertex1) && this.graph.has(vertex2);
 
-    if (areNodesExisting) {
-      this.graph.get(vertex2).push(vertex1)
-      if (!this.isDirected) {
-        this.graph.get(vertex1).push(vertex2)
-      }
-    }
-    console.log(this.graph)
-  }
-}
+//     if (areNodesExisting) {
+//       this.graph.get(vertex2).push(vertex1);
+//       if (!this.isDirected) {
+//         this.graph.get(vertex1).push(vertex2);
+//       }
+//     }
+//   }
 
-const myGraph = new Graph(true)
+//   print() {
+//     console.log(`Graph is ${this.isDirected ? "directed" : "undirected"}`);
+//     for (let vertex of this.graph) {
+//       console.log(
+//         `Value ${vertex[0]} --> ${
+//           vertex[1].length ? "Deps:" : "No deps"
+//         } ${vertex[1].map((dep) => dep)}`
+//       );
+//     }
+//   }
 
-myGraph.addVertex(100)
-myGraph.addVertex(300)
-myGraph.addVertex(200)
+//   drawSVG() {
+//     function drawCircle(x, y, radius, color, text) {
+//       return `<circle cx="${x}" cy="${y}" r="${radius}" fill="${color}">
+//       </circle>
+//       <text x="${x}" y="${y}" fill="black">
+//       ${text}
+//       </text>`;
+//     }
 
-myGraph.addEdge(100, 200)
-myGraph.addEdge(300, 200)
+//     function drawLine(x1, y1, x2, y2, color) {
+//       return `<line x1="${x1}" y1="${y1}" x2="${x2}" y2="${y2}" stroke="${color}" />`;
+//     }
+//     const svgElements = [];
 
-// myGraph.print()
+//     for (let vertex of this.graph) {
+//       const radius = 10;
+//       // add vertex
+//       svgElements.push(
+//         drawCircle(
+//           vertex[0] + radius,
+//           vertex[0] + radius,
+//           radius,
+//           "orange",
+//           vertex[0]
+//         )
+//       );
 
+//       // add deps
+//       if (vertex[1].length) {
+//         svgElements.push(
+//           vertex[1]
+//             .map((dep) => {
+//               const color = Math.random() > 0.5 ? "blue" : "red";
+//               return `${drawLine(
+//                 vertex[0] + radius,
+//                 vertex[0] + radius,
+//                 dep + radius,
+//                 dep + radius,
+//                 color
+//               )}`;
+//             })
+//             .join("")
+//         );
+//       }
+//     }
+//     console.log(svgElements);
+//     const svgContent = svgElements.join("");
+//     const svgOutput = `<svg width="800" height="800">${svgContent}</svg>`;
+//     console.log(svgOutput);
+//     document.body.innerHTML = svgOutput;
+//   }
+// }
 
+// const myGraph = new Graph(true);
 
+// myGraph.addVertex(100);
+// myGraph.addVertex(350);
+// myGraph.addVertex(180);
 
+// myGraph.addEdge(100, 180);
+// myGraph.addEdge(350, 180);
 
-
-
-
-
-
+// myGraph.print();
+// myGraph.drawSVG();
 
 // ## Создать класс для удобной работы с графом
 
